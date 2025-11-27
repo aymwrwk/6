caf338179f8aa0cea8b070fb27bf6b57bbe032f8
Bruno Nelio <whosbin@gmail.com>
2025-11-27 14:36:44 -0300

    ><

--- netlify/functions/gerarPagamento.js
+++ netlify/functions/gerarPagamento.js
@@ -1,12 +1,21 @@
 exports.handler = async (event, context) => {
     const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-    // Coloque seus dados reais aqui!!!
     const clientId = "cc405852-0aba-436a-bf91-b30f35322e85";
     const clientSecret = "G3nJUgysggOrP6KsZh9QJGeDDkcwbyRZfyXT/A2oJFSigty6RgqLm/ThzCIZ5A2dt1o7CQwoWZoEWauwAksxDnZRsLwQoaFGJwFMLnq056+QthSFUjEhLb6tXoPxBUDxhf6Q1fQshM7oxvJu7hT28dmQpWV7JJ1ybmfO2QKTruY";
 
-    const valor = 7.90;
-    const descricao = "Compra no site";
+    // pega os dados enviados pelo front-end
+    const body = JSON.parse(event.body || "{}");
+
+    const valor = Number(body.item?.price?.replace("R$", "").replace(",", "."));
+    const descricao = body.item?.title || "Compra no site";
+
+    if (!valor) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "Valor inválido" })
+        };
+    }
 
     try {
         const response = await fetch("https://api.livepix.gg/v1/charge", {
@@ -37,3 +46,5 @@ exports.handler = async (event, context) => {
         };
     }
 };
+
+
