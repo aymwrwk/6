cd37790b04b97ed912987abf01fc0184b0de3783
Bruno Nelio <whosbin@gmail.com>
2025-11-28 12:03:27 -0300

    ><

--- netlify/functions/gerar-pagamento.js
+++ netlify/functions/gerar-pagamento.js
@@ -1,28 +1,12 @@
-const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
-
 exports.handler = async (event) => {
   try {
     const { valor, descricao } = JSON.parse(event.body);
 
-    // 1. PEGAR TOKEN DE ACESSO
-    const auth = await fetch("https://api.livepix.gg/oauth/token", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        client_id: process.env.LIVEPIX_CLIENT_ID,
-        client_secret: process.env.LIVEPIX_CLIENT_SECRET,
-        grant_type: "client_credentials"
-      })
-    });
-
-    const { access_token } = await auth.json();
-
-    // 2. CRIAR PAGAMENTO
-    const response = await fetch("https://api.livepix.gg/payments", {
+    const response = await fetch("https://api.livepix.gg/payment", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${access_token}`
+        "Authorization": `Bearer ${process.env.LIVEPIX_API_KEY}`
       },
       body: JSON.stringify({
         amount: valor,
