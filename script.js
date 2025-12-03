96d8b1aeb6830f940df0aeaf9db0e8f666fb5be4
Bruno Nelio <whosbin@gmail.com>
2025-12-03 14:33:27 -0300

    ><

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-// ANIMAÇÃO DOS PRODUTOS AO APARECER
-const produtos = document.querySelectorAll(".produto-card");
+let index = 0;
+const images = document.querySelectorAll(".carousel img");
 
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) entry.target.classList.add("show");
-    });
-});
+function showImage(i) {
+    images.forEach(img => img.classList.remove("active"));
+    images[i].classList.add("active");
+}
 
-produtos.forEach(prod => observer.observe(prod));
+document.getElementById("prev").onclick = () => {
+    index = (index === 0) ? images.length - 1 : index - 1;
+    showImage(index);
+};
 
-
-// BARRA DE PESQUISA
-const searchInput = document.getElementById("searchInput");
-const produtosContainer = document.getElementById("produtosContainer");
-
-searchInput.addEventListener("keyup", () => {
-    const termo = searchInput.value.toLowerCase();
-
-    document.querySelectorAll(".produto-card").forEach(card => {
-        const nome = card.getAttribute("data-name").toLowerCase();
-
-        if (nome.includes(termo)) {
-            card.style.display = "block";
-        } else {
-            card.style.display = "none";
-        }
-    });
-});
+document.getElementById("next").onclick = () => {
+    index = (index === images.length - 1) ? 0 : index + 1;
+    showImage(index);
+};
